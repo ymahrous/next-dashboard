89e956df11cf3bd9a4914e2bfd2fbf78c1defa73
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-06-30 15:31:03 +0300

    init

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 export default function Page() {
   return (
@@ -21,10 +21,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
+          <Link href="/login" className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
